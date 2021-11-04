8b6a5f18da2df6c960164b366459d0d6457dfc59
rollwagen <rollwagen@gmail.com>
2021-11-04 20:28:48 +0100

    Fix flake8 errors

--- aws_account/test/test_cli.py
+++ aws_account/test/test_cli.py
@@ -1,6 +1,5 @@
-import pytest
+from aws_account.cli import AWSIdentity, AWSAccount
 
-from aws_account.cli import *
 
 def test_awsaccount_basic():
     account = AWSAccount("id", "name", "email@email.com")
@@ -8,16 +7,14 @@ def test_awsaccount_basic():
     assert account.name == "name"
     assert account.email == "email@email.com"
 
+
 def test_awsidentity_with_iamuser():
     arn = "arn='arn:aws:iam::11122233334444:user/superadmin'"
-    account = '11122233334444'
-    user_id = 'CR4ZEACOVAIDASXAYEZNC'
+    account = "11122233334444"
+    user_id = "CR4ZEACOVAIDASXAYEZNC"
 
     identity = AWSIdentity(user_id=user_id, account=account, arn=arn)
 
     assert identity.is_iam()
-    assert identity.is_assumed_role() == False
+    assert identity.is_assumed_role() is False
     assert identity.type == AWSIdentity.Type.IAM
-
-
-
