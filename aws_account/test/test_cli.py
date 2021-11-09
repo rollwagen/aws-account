075a9fd757b2a8c04f870e2f96a45e2dc350ef83
rollwagen <rollwagen@gmail.com>
2021-11-09 09:11:52 +0100

    Add testcase for issue #4

--- aws_account/test/test_cli.py
+++ aws_account/test/test_cli.py
@@ -1,4 +1,7 @@
-from aws_account.cli import AWSIdentity, AWSAccount
+from aws_account.cli import AWSIdentity, AWSAccount, _get_access_token
+
+from unittest import mock
+from unittest.mock import patch
 
 
 def test_awsaccount_basic():
@@ -18,3 +21,37 @@ def test_awsidentity_with_iamuser():
     assert identity.is_iam()
     assert identity.is_assumed_role() is False
     assert identity.type == AWSIdentity.Type.IAM
+
+
+@patch(
+    "builtins.open",
+    new_callable=mock.mock_open,
+    read_data='{"startUrl": "https://a123.awsapps.com/start", '
+    + '"region": "eu-central-1", "accessToken": "pzFA3BZ0s7Q4xHGv", '
+    + '"expiresAt": "2021-11-09T02:58:40Z"}',
+)
+@patch(
+    "os.listdir",
+    return_value=[
+        "aws-toolkit-jetbrains-client-id-eu-central-9.json",
+        "09042c3fff60f06bad40477bd3d1fc628034320e.json",
+        "botocore-client-id-eu-central-1.json",
+    ],
+)
+@patch("os.path.expanduser", return_value="/Users/test/.aws/sso/cache")
+def test_get_access_token(expanduser, listdir, builtins_open):
+    class FileValidator(object):
+        def __init__(
+            self, validator
+        ):  # validator=function taking a single argument and returns a bool.
+            self.validator = validator
+
+        def __eq__(self, other):
+            return bool(self.validator(other))
+
+        token = _get_access_token()
+        builtins_open.assert_called_with(
+            "/Users/test/.aws/sso/cache/09042c3fff60f06bad40477bd3d1fc628034320e.json",
+            "r",
+        )
+        assert token == "pzFA3BZ0s7Q4xHGv"
